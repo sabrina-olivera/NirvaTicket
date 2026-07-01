6231ea1173c21c2c259030ae928854e9d9f7f119
sabrina-olivera <sabrinayamila@gmail.com>
2026-07-01 02:56:35 -0300

    add ticket component

--- NirvaTicket/frontend/src/App.jsx
+++ NirvaTicket/frontend/src/App.jsx
@@ -2,14 +2,15 @@ import './App.css'
 import Main from './components/Main';
 import Header from './components/Header';
 import ProductList from './components/ProductList/ProductList';
+import Ticket from './components/Ticket/Ticket';
 
 
 function App() {
-
   return (
     <>
       <Header />
-      <ProductList />
+      {/* <ProductList /> */}
+      <Ticket />
     </>
   )
 }
